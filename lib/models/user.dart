76904c5895d9904082324ea9b070b47dffa66436
AntonLipovtsev <lipovtsev.anton@yandex.by>
2020-08-19 18:26:42 +0300

    [ADD] - task 2;

---
+++ lib/models/user.dart
@@ -0,0 +1,113 @@
+import 'package:FlutterGalleryApp/string_utils.dart';
+
+class User {
+  String email;
+  String phone;
+
+  String _firstName;
+  String _lastName;
+
+  LoginType _loginType;
+
+  List<User> friends = List<User>();
+
+  User._({String firstName, String lastName, String phone, String email})
+      : _firstName = firstName,
+        _lastName = lastName,
+        this.phone = phone,
+        this.email = email {
+    _loginType = email != null ? LoginType.EMAIL : LoginType.PHONE;
+  }
+
+  factory User({String name, String phone, String email}) {
+    if (name.isEmpty) {
+      throw Exception("User name is empty");
+    }
+
+    return User._(
+        firstName: _getFirstName(name),
+        lastName: _getLastName(name),
+        phone: checkPhone(phone),
+        email: email);
+  }
+
+  static String _getFirstName(String userName) => userName.split(" ")[0];
+
+  static String _getLastName(String userName) => userName.split(" ")[1];
+
+  static String checkPhone(String phone) {
+    if (phone == null) {
+      return phone;
+    }
+
+    String pattern = r"^(?:[+0])?[0-9]{11}";
+
+    phone = phone.replaceAll(RegExp("[^+\\d]"), "");
+
+    if (phone.isEmpty) {
+      throw Exception("Enter don't empty phone number");
+    } else if (!RegExp(pattern).hasMatch(phone)) {
+      throw Exception("Enter a valid phone number starting with a + and containing 11 digits");
+    }
+
+    return phone;
+  }
+
+  static String checkEmail(String email) {
+    if (email == null || email.isEmpty) {
+      throw Exception("Email is empty");
+    }
+
+    return email;
+  }
+
+  String get login {
+    if (_loginType == LoginType.EMAIL) {
+      return email;
+    } else {
+      return phone;
+    }
+  }
+
+  String get name => "${_firstName.capitalize()} ${_lastName.capitalize()}";
+
+  void addFriends(Iterable<User> newFriends) {
+    friends.addAll(newFriends);
+  }
+
+  void removeFriend(User user) {
+    friends.remove(user);
+  }
+
+  String get userInfo => '''
+    name: $name
+    email: $email
+    firstName: $_firstName
+    lastName: $_lastName
+    friends: ${friends.toList()}
+  ''';
+
+  @override
+  String toString() {
+    return '''
+    name: $name
+    email: $email
+    friends: ${friends.toList()}
+  ''';
+  }
+
+  @override
+  bool operator ==(Object object) {
+    if (object == null) {
+      return false;
+    }
+
+    if (object is User) {
+      return _firstName == object._firstName &&
+          _lastName == object._lastName &&
+          (phone == object.phone || email == object.email);
+    }
+  }
+}
+
+enum LoginType { EMAIL, PHONE }
---
+++ lib/user_holder.dart
@@ -0,0 +1,84 @@
+import 'package:FlutterGalleryApp/models/user.dart';
+
+class UserHolder {
+  Map<String, User> users = {};
+
+  void registerUser(String name, String phone, String email) {
+    var user = User(name: name, phone: phone, email: email);
+
+    if (!users.containsKey(user.login)) {
+      users[user.login] = user;
+    } else {
+      throw Exception("A user with this email already exists");
+    }
+  }
+
+  User registerUserByEmail(String fullName, String email) {
+    if (!email.contains("@")) {
+      throw Exception();
+    }
+
+    var user = User(name: fullName, email: email);
+
+    if (!users.containsKey(user.login)) {
+      users[user.login] = user;
+    } else {
+      throw Exception("A user with this email already exists");
+    }
+
+    return user;
+  }
+
+  User registerUserByPhone(String fullName, String phone) {
+    if (phone == null || phone.isEmpty) {
+      throw Exception("Enter don't empty phone number");
+    }
+
+    var user = User(name: fullName, phone: phone);
+
+    if (!users.containsKey(user.login)) {
+      users[user.login] = user;
+    } else {
+      throw Exception("A user with this phone already exists");
+    }
+
+    return user;
+  }
+
+  User findUserInFriends(String fullName, User user) {
+    User necessaryFriend;
+
+    users[fullName].friends.forEach((friend) {
+      if (friend == user) {
+        necessaryFriend = user;
+      }
+    });
+
+    if (necessaryFriend == null) {
+      throw Exception("${user.login} is not a friend of the login");
+    }
+
+    return necessaryFriend;
+  }
+
+  void setFriends(String login, List<User> friends) {
+    users[login].friends = friends;
+  }
+
+  User getUserByLogin(String login) {
+    return users[login];
+  }
+
+  List<User> importUsers(List<String> list) {
+    var users = List<User>();
+
+    list.forEach((element) {
+      String name = element.split(";")[0].trim();
+      String email = element.split(";")[1].trim();
+      String phone = element.split(";")[2].trim();
+      users.add(User(name: name, email: email, phone: phone));
+    });
+
+    return users;
+  }
+}
